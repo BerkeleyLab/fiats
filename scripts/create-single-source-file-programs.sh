97df78b49c43fbef1008cbaccd7c47262957f6a5
Damian Rouson <rouson@lbl.gov>
2024-11-25 01:35:07 -0500

    chore(script): write single-file source to build/

--- scripts/create-single-source-file-programs.sh
+++ scripts/create-single-source-file-programs.sh
@@ -7,6 +7,14 @@
 export assert_dir="../build/dependencies/assert"
 export julienne_dir="../build/dependencies/julienne"
 export fiats_dir=".."
+export destination_dir="../build/single-file-programs"
+
+if [ -d $destination_dir ]; then
+  echo "Destination directory exists: $destination_dir"
+else
+  echo "Creating destination directory: $destination_dir"
+  mkdir "$destination_dir"
+fi
 
 echo ""
 echo "---------------------- create-single-source-file.sh ----------------------------"
@@ -31,22 +39,22 @@ fi
 echo "Concatenating Assert." 
 assert_modules=$(find "$assert_dir/src" -name '*_m.?90')
 assert_submodules=$(find "$assert_dir/src" -name '*_s.?90')
-cat ${assert_include:-} $assert_modules $assert_submodules > assert.F90
+cat $assert_modules $assert_submodules > "$destination_dir"/assert.F90
 
 echo "Concatenating Julienne." 
 julienne_modules=$(find "$julienne_dir/src" -name '*_m.?90')
 julienne_submodules=$(find "$julienne_dir/src" -name '*_s.?90')
-cat $julienne_modules $julienne_submodules > julienne.F90
+cat $julienne_modules $julienne_submodules > "$destination_dir"/julienne.F90
 
 echo "Concatenating Fiats." 
 fiats_modules=$(find "$fiats_dir/src" -name '*_m.?90')
 fiats_submodules=$(find "$fiats_dir/src" -name '*_s.?90')
-cat $fiats_modules $fiats_submodules > fiats.F90
+cat $fiats_modules $fiats_submodules > "$destination_dir"/fiats.F90
 
 echo "Copying include files." 
-cp "$assert_dir/include/assert_macros.h" .
-cat "$julienne_dir/include/language-support.F90" "$fiats_dir/include/language-support.F90" > language-support.F90
+cp "$assert_dir/include/assert_macros.h" "$destination_dir"
+cat "$julienne_dir/include/language-support.F90" "$fiats_dir/include/language-support.F90" > "$destination_dir"/language-support.F90
+cp "$fiats_dir/include/compound_assertions.h" "$destination_dir"
 
-
-echo "Concatenating Assert, Julienne, Fiats, and example/concurrent-inferences.f90." 
-cat assert.F90 julienne.F90 fiats.F90 "$fiats_dir/example/concurrent-inferences.f90" > concurrent-inferences-single-file.F90
+echo "Concatenating Assert, Julienne, Fiats, and example/concurrent-inferences.f90 into $destination_dir"
+cat "$destination_dir"/assert.F90 "$destination_dir"/julienne.F90 "$destination_dir"/fiats.F90 "$fiats_dir/example/concurrent-inferences.f90" > $destination_dir/concurrent-inferences-single-file.F90
