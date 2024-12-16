49a416f59238b960e346cb84df4c48b0713dda05
Damian Rouson <rouson@lbl.gov>
2024-12-15 23:50:05 -0800

    repro(double_precison_string): rm unneeded file

--- src/double_precision_string_m.f90
+++
@@ -1,6 +0,0 @@
-module double_precision_string_m
-  use julienne_m, only : string_t
-  implicit none
-  type, extends(string_t) :: double_precision_string_t
-  end type
-end module double_precision_string_m
