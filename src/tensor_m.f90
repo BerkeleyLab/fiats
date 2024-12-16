9e2946eb436021afcc528efada785198f4c982f6
Damian Rouson <rouson@lbl.gov>
2024-12-15 23:57:14 -0800

    repro(tensor): rm unneeded file

--- src/tensor_m.f90
+++
@@ -1,8 +0,0 @@
-module tensor_m
-  use kind_parameters_m, only : default_real, double_precision
-  implicit none
-  type tensor_t(k)
-    integer, kind :: k = default_real 
-    real(k), allocatable, private :: values_(:)
-  end type
-end module
