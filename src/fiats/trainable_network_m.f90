d23ebd6f59f317c9d95474060ba18fd010a81c7e
Damian Rouson <rouson@lbl.gov>
2025-02-06 10:26:14 -0800

    repro(trainable_network): simplify constructor

--- src/fiats/trainable_network_m.f90
+++ src/fiats/trainable_network_m.f90
@@ -35,46 +35,26 @@ module trainable_network_m
 
 contains
 
-    pure function default_real_network(neural_network) result(trainable_network)
-      implicit none
-      type(neural_network_t), intent(in) :: neural_network
-      type(trainable_network_t) trainable_network
-      trainable_network%neural_network_t = neural_network
-    end function 
+  pure function default_real_network(neural_network) result(trainable_network)
+    implicit none
+    type(neural_network_t), intent(in) :: neural_network
+    type(trainable_network_t) trainable_network
+    trainable_network%neural_network_t = neural_network
+  end function 
 
   module procedure perturbed_identity_network
 
-    integer k, l
-    real, allocatable :: identity(:,:,:), w_harvest(:,:,:), b_harvest(:,:)
+    real, allocatable :: w(:,:,:), b(:,:)
 
     associate(n=>training_configuration%nodes_per_layer())
       associate(n_max => maxval(n), layers => size(n))
-
-        identity = reshape( [( [(e(k,n_max), k=1,n_max)], l = 1, layers-1 )], [n_max, n_max, layers-1])
-        allocate(w_harvest, mold = identity)
-        allocate(b_harvest(size(identity,1), size(identity,3)))
-        call random_number(w_harvest)
-        call random_number(b_harvest)
-
-        associate( &
-          w => identity + perturbation_magnitude*(w_harvest-0.5)/0.5, &
-          b => perturbation_magnitude*(b_harvest-0.5)/0.5 &
+        allocate(w(n_max,n_max,layers-1), source = 0.)
+        allocate(b(size(w,1), size(w,3)), source = 0.)
+        trainable_network = default_real_network( &
+          neural_network_t(nodes=n, weights=w, biases=b, metadata=metadata, input_map=input_map, output_map=output_map) &
         )
-          trainable_network = default_real_network( &
-            neural_network_t(nodes=n, weights=w, biases=b, metadata=metadata, input_map=input_map, output_map=output_map) &
-          )
-        end associate
       end associate
     end associate
-
-  contains
-
-    pure function e(j,n) result(unit_vector)
-      integer, intent(in) :: j, n
-      integer k
-      real, allocatable :: unit_vector(:)
-      unit_vector = real([(merge(1,0,j==k),k=1,n)])
-    end function
 
   end procedure
 
