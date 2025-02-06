a853c95839ab0e5bd55472d8b2cf38ef64ab078c
Damian Rouson <rouson@lbl.gov>
2025-02-06 10:31:39 -0800

    repro(trainable_network): simplify

--- src/fiats/trainable_network_m.f90
+++ src/fiats/trainable_network_m.f90
@@ -3,35 +3,16 @@
 
 module trainable_network_m
   use neural_network_m, only : neural_network_t
-  use input_output_pair_m, only : input_output_pair_t
   use julienne_m, only : string_t
   use kind_parameters_m, only : default_real
-  use mini_batch_m, only : mini_batch_t
   use training_configuration_m, only : training_configuration_t
   use tensor_map_m, only : tensor_map_t
   implicit none
-
-  private
-  public :: trainable_network_t 
 
   type, extends(neural_network_t) ::  trainable_network_t(m)
     integer, kind :: m = default_real
     private
   end type
-
-  interface trainable_network_t 
-
-    module function perturbed_identity_network(training_configuration, perturbation_magnitude, metadata, input_map, output_map) &
-      result(trainable_network)
-      implicit none
-      type(training_configuration_t), intent(in) :: training_configuration
-      real, intent(in) :: perturbation_magnitude
-      type(string_t), intent(in) :: metadata(:)
-      type(tensor_map_t) input_map, output_map
-      type(trainable_network_t) trainable_network
-    end function
-
-  end interface
 
 contains
 
@@ -42,7 +23,14 @@ contains
     trainable_network%neural_network_t = neural_network
   end function 
 
-  module procedure perturbed_identity_network
+  function perturbed_identity_network(training_configuration, perturbation_magnitude, metadata, input_map, output_map) &
+      result(trainable_network)
+      implicit none
+      type(training_configuration_t), intent(in) :: training_configuration
+      real, intent(in) :: perturbation_magnitude
+      type(string_t), intent(in) :: metadata(:)
+      type(tensor_map_t) input_map, output_map
+      type(trainable_network_t) trainable_network
 
     real, allocatable :: w(:,:,:), b(:,:)
 
@@ -56,6 +44,6 @@ contains
       end associate
     end associate
 
-  end procedure
+  end function
 
 end module trainable_network_m
