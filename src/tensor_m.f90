4b196324a3cb87bbef3622f54304bd174e2abb10
Damian Rouson <rouson@lbl.gov>
2025-02-17 22:32:47 -0800

    repro(tensor_): rm type-bound procedures

--- src/tensor_m.f90
+++ src/tensor_m.f90
@@ -1,20 +1,9 @@
-! Copyright (c), The Regents of the University of California
-! Terms of use are as specified in LICENSE.txt
 module tensor_m
-  use kind_parameters_m, only : default_real, double_precision
   implicit none
 
-  private
-  public :: tensor_t
-
   type tensor_t(k)
-    integer, kind :: k = default_real 
+    integer, kind :: k = kind(1.)
     real(k), allocatable, private :: values_(:)
-  contains
-    generic   :: values => default_real_values, double_precision_values
-    procedure, private, non_overridable ::  default_real_values, double_precision_values
-    generic :: num_components => default_real_num_components, double_precision_num_components
-    procedure, private ::        default_real_num_components, double_precision_num_components
   end type
 
   interface tensor_t
@@ -33,32 +22,4 @@ module tensor_m
 
   end interface
 
-  interface
-
-    pure module function default_real_values(self) result(tensor_values)
-      implicit none
-      class(tensor_t), intent(in) :: self
-      real, allocatable :: tensor_values(:)
-    end function
-
-    pure module function double_precision_values(self) result(tensor_values)
-      implicit none
-      class(tensor_t(double_precision)), intent(in) :: self
-      double precision, allocatable :: tensor_values(:)
-    end function
-
-    pure module function default_real_num_components(self) result(n)
-      implicit none
-      class(tensor_t), intent(in) :: self
-      integer n
-    end function
-
-    pure module function double_precision_num_components(self) result(n)
-      implicit none
-      class(tensor_t(double_precision)), intent(in) :: self
-      integer n
-    end function
-
-  end interface
-
-end module tensor_m
+end module
