1b27ea9f7890c9ee68c7ba419c073196b1fdfa22
Damian Rouson <rouson@lbl.gov>
2025-02-17 23:01:31 -0800

    repro: rm tensor_map_m

--- src/neural_network_m.f90
+++ src/neural_network_m.f90
@@ -2,12 +2,10 @@ module neural_network_m
   use activation_m, only : activation_t
   use julienne_m, only : string_t
   use metadata_m, only : metadata_t
-  use tensor_map_m, only : tensor_map_t
   implicit none
 
   type neural_network_t(k)
     integer, kind :: k = kind(1.)
-    type(tensor_map_t(k)), private :: input_map_, output_map_
     type(metadata_t), private :: metadata_
     real(k), allocatable, private :: weights_(:,:,:), biases_(:,:)
     integer, allocatable, private :: nodes_(:)
@@ -33,13 +31,12 @@ module neural_network_m
 
   interface neural_network_t
 
-    module function default_real_construct_from_components(metadata, weights, biases, nodes, input_map, output_map) &
+    module function default_real_construct_from_components(metadata, weights, biases, nodes) &
       result(neural_network)
       implicit none
       type(string_t), intent(in) :: metadata(:)
       real, intent(in) :: weights(:,:,:), biases(:,:)
       integer, intent(in) :: nodes(0:)
-      type(tensor_map_t), intent(in), optional :: input_map, output_map
       type(neural_network_t) neural_network
     end function
 
