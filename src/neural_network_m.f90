bb7a5f4bc1f61371fe3e4a4546825ef863e190c1
Damian Rouson <rouson@lbl.gov>
2025-02-18 00:52:56 -0800

    repro: combine{neural,trainable}_network.f90

--- src/neural_network_m.f90
+++ src/neural_network_m.f90
@@ -27,3 +27,24 @@ module neural_network_m
   end interface
 
 end module
+module trainable_network_m
+  use neural_network_m
+  implicit none
+
+  type, extends(neural_network_t) ::  trainable_network_t(m)
+    integer, kind :: m = kind(1.)
+  end type
+
+  interface trainable_network_t
+    pure module function default_real_network(neural_network) result(trainable_network)
+      implicit none
+      type(neural_network_t), intent(in) :: neural_network
+      type(trainable_network_t) trainable_network
+    end function
+  end interface
+
+contains
+  module procedure default_real_network
+    trainable_network%neural_network_t = neural_network
+  end procedure
+end module
