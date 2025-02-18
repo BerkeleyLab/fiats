2a4ff7f5756a9041a0a33fd259c1d8e7efae8422
Damian Rouson <rouson@lbl.gov>
2025-02-18 12:41:20 -0800

    repro: make biases a scalar

--- all.f90
+++ all.f90
@@ -3,7 +3,7 @@ module neural_network_m
 
   type neural_network_t(k)
     integer, kind :: k = kind(1.)
-    real(k), allocatable :: biases_(:)
+    real(k), allocatable :: biases
   end type
 
   type, extends(neural_network_t) ::  trainable_network_t(m)
@@ -37,6 +37,6 @@ end module
 contains
   function trainable_network()
     type(trainable_network_t) trainable_network
-    trainable_network = trainable_network_t(neural_network_t(biases = [0.]))
+    trainable_network = trainable_network_t(neural_network_t(biases = 0.))
   end function
 end 
