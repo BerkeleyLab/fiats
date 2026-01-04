31dcd5bd88bb52965407eaf7b6180c9c0e9247d2
Damian Rouson <damian@rouson.net>
2026-01-03 21:59:28 -0800

    repro: rm unnecessary function invocations

--- test/driver.f90
+++ test/driver.f90
@@ -5,14 +5,31 @@ program test_suite_driver
 
   type(training_configuration_t) training_configuration 
 
-  training_configuration = training_configuration_t( &
-    hyperparameters_t(mini_batches=5, learning_rate=1., optimizer = "adam") &
-   ,network_configuration_t(skip_connections=.false., nodes_per_layer=[2,72,2], activation_name="sigmoid") &
-   ,tensor_names_t(inputs=[string_t("pressure"), string_t("temperature")], outputs=[string_t("saturated mixing ratio")]) &
-  )
   block
     type(training_configuration_t) from_json
-    from_json = training_configuration_t(file_t(training_configuration%to_json()))
+    type(file_t) json_file
+
+    json_file = file_t([ &
+       string_t('{') &
+      ,string_t('    "hyperparameters": {') &
+      ,string_t('        "mini-batches" : 5,') &
+      ,string_t('        "learning rate" : 1.00000000,') &
+      ,string_t('        "optimizer" : "adam"') &
+      ,string_t('    }') &
+      ,string_t(',') &
+      ,string_t('    "network configuration": {') &
+      ,string_t('        "skip connections" : false,') &
+      ,string_t('        "nodes per layer" : [2,                       72,                        2],') &
+      ,string_t('        "activation function" : "sigmoid"') &
+      ,string_t('    }') &
+      ,string_t(',') &
+      ,string_t('    "tensor names": {') &
+      ,string_t('        "inputs"  : ["pressure","temperature"],') &
+      ,string_t('        "outputs" : ["saturated mixing ratio"]') &
+      ,string_t('    }') &
+      ,string_t('}') &
+    ])
+    from_json = training_configuration_t(json_file)
   end block
 
 end program
