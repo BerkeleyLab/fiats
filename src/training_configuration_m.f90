faac435902222cecd64af1ba2d48764577d80370
Damian Rouson <damian@rouson.net>
2026-01-03 20:07:02 -0800

    repro(training_config): rm all double_prec funcs

--- src/training_configuration_m.f90
+++ src/training_configuration_m.f90
@@ -1,5 +1,3 @@
-! Copyright (c) 2023-2025, The Regents of the University of California
-! Terms of use are as specified in LICENSE.txt
 module training_configuration_m
   use activation_m, only : activation_t
   use julienne_string_m, only : string_t
@@ -20,26 +18,26 @@ module training_configuration_m
     type(network_configuration_t), private :: network_configuration_
     type(tensor_names_t), private :: tensor_names_
   contains
-    generic :: operator(==)     => default_real_equals           , double_precision_equals
-    procedure, private          :: default_real_equals           , double_precision_equals
-    generic :: to_json          => default_real_to_json          , double_precision_to_json
-    procedure, private          :: default_real_to_json          , double_precision_to_json
-    generic :: mini_batches     => default_real_mini_batches     , double_precision_mini_batches
-    procedure, private          :: default_real_mini_batches     , double_precision_mini_batches
-    generic :: optimizer_name   => default_real_optimizer_name   , double_precision_optimizer_name
-    procedure, private          :: default_real_optimizer_name   , double_precision_optimizer_name
-    generic :: learning_rate    => default_real_learning_rate    , double_precision_learning_rate
-    procedure, private          :: default_real_learning_rate    , double_precision_learning_rate
-    generic :: activation       => default_real_activation       , double_precision_activation
-    procedure, private          :: default_real_activation       , double_precision_activation
-    generic :: nodes_per_layer  => default_real_nodes_per_layer  , double_precision_nodes_per_layer
-    procedure, private          :: default_real_nodes_per_layer  , double_precision_nodes_per_layer
-    generic :: skip_connections => default_real_skip_connections , double_precision_skip_connections
-    procedure, private ::          default_real_skip_connections , double_precision_skip_connections
-    generic :: input_variable_names  => default_real_input_names , double_precision_input_names
-    procedure, private ::               default_real_input_names , double_precision_input_names
-    generic :: output_variable_names => default_real_output_names, double_precision_output_names
-    procedure, private ::               default_real_output_names, double_precision_output_names
+    generic :: operator(==)     => default_real_equals           
+    procedure, private          :: default_real_equals           
+    generic :: to_json          => default_real_to_json          
+    procedure, private          :: default_real_to_json          
+    generic :: mini_batches     => default_real_mini_batches     
+    procedure, private          :: default_real_mini_batches     
+    generic :: optimizer_name   => default_real_optimizer_name   
+    procedure, private          :: default_real_optimizer_name   
+    generic :: learning_rate    => default_real_learning_rate    
+    procedure, private          :: default_real_learning_rate    
+    generic :: activation       => default_real_activation       
+    procedure, private          :: default_real_activation       
+    generic :: nodes_per_layer  => default_real_nodes_per_layer  
+    procedure, private          :: default_real_nodes_per_layer  
+    generic :: skip_connections => default_real_skip_connections 
+    procedure, private ::          default_real_skip_connections 
+    generic :: input_variable_names  => default_real_input_names 
+    procedure, private ::               default_real_input_names 
+    generic :: output_variable_names => default_real_output_names
+    procedure, private ::               default_real_output_names
   end type
   interface training_configuration_t
 
@@ -52,25 +50,10 @@ module training_configuration_m
       type(tensor_names_t), intent(in) :: tensor_names
     end function
 
-    pure module function double_precision_from_components(hyperparameters, network_configuration, tensor_names) &
-      result(training_configuration)
-      implicit none
-      type(hyperparameters_t(double_precision)), intent(in) :: hyperparameters
-      type(network_configuration_t), intent(in) :: network_configuration
-      type(tensor_names_t), intent(in) :: tensor_names
-      type(training_configuration_t(double_precision)) training_configuration
-    end function
-
     module function default_real_from_file(file_object) result(training_configuration)
       implicit none
       type(file_t), intent(in) :: file_object
       type(training_configuration_t) training_configuration
-    end function
-
-    module function double_precision_from_file(file_object) result(training_configuration)
-      implicit none
-      type(double_precision_file_t), intent(in) :: file_object
-      type(training_configuration_t(double_precision)) training_configuration
     end function
 
   end interface
@@ -83,21 +66,9 @@ module training_configuration_m
       type(string_t), allocatable :: json_lines(:)
     end function
 
-    pure module function double_precision_to_json(self) result(json_lines)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      type(string_t), allocatable :: json_lines(:)
-    end function
-
     elemental module function default_real_equals(lhs, rhs) result(lhs_eq_rhs)
       implicit none
       class(training_configuration_t), intent(in) :: lhs, rhs
-      logical lhs_eq_rhs
-    end function
-
-    elemental module function double_precision_equals(lhs, rhs) result(lhs_eq_rhs)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: lhs, rhs
       logical lhs_eq_rhs
     end function
 
@@ -107,21 +78,9 @@ module training_configuration_m
       integer num_mini_batches
     end function
 
-    elemental module function double_precision_mini_batches(self) result(num_mini_batches)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      integer num_mini_batches
-    end function
-
     elemental module function default_real_optimizer_name(self) result(identifier)
       implicit none
       class(training_configuration_t), intent(in) :: self
-      type(string_t) identifier
-    end function
-
-    elemental module function double_precision_optimizer_name(self) result(identifier)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
       type(string_t) identifier
     end function
 
@@ -131,69 +90,33 @@ module training_configuration_m
       real rate
     end function
  
-    elemental module function double_precision_learning_rate(self) result(rate)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      double precision rate
-    end function
-
     module function default_real_activation(self) result(activation)
       implicit none
       class(training_configuration_t), intent(in) :: self
       type(activation_t) activation
     end function
 
-    module function double_precision_activation(self) result(activation)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      type(activation_t) activation
-    end function
- 
     pure module function default_real_nodes_per_layer(self) result(nodes)
       implicit none
       class(training_configuration_t), intent(in) :: self
       integer, allocatable :: nodes(:)
     end function
 
-    pure module function double_precision_nodes_per_layer(self) result(nodes)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      integer, allocatable :: nodes(:)
-    end function
- 
     elemental module function default_real_skip_connections(self) result(using_skip)
       implicit none
       class(training_configuration_t), intent(in) :: self
       logical using_skip
     end function
 
-    elemental module function double_precision_skip_connections(self) result(using_skip)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      logical using_skip
-    end function
- 
     pure module function default_real_input_names(self) result(input_names)
       implicit none
       class(training_configuration_t), intent(in) :: self
       type(string_t), allocatable :: input_names(:)
     end function
 
-    pure module function double_precision_input_names(self) result(input_names)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
-      type(string_t), allocatable :: input_names(:)
-    end function
-
     pure module function default_real_output_names(self) result(output_names)
       implicit none
       class(training_configuration_t), intent(in) :: self
-      type(string_t), allocatable :: output_names(:)
-    end function
-
-    pure module function double_precision_output_names(self) result(output_names)
-      implicit none
-      class(training_configuration_t(double_precision)), intent(in) :: self
       type(string_t), allocatable :: output_names(:)
     end function
 
--- src/training_configuration_s.F90
+++ src/training_configuration_s.F90
@@ -27,23 +27,6 @@ contains
 
   end procedure
 
-  module procedure double_precision_from_components
-
-    training_configuration%hyperparameters_ = hyperparameters
-    training_configuration%network_configuration_ = network_configuration
-    training_configuration%tensor_names_ = tensor_names
-
-    training_configuration%file_t = file_t([ &
-      string_t(header), &
-      training_configuration%hyperparameters_%to_json(), &
-      string_t(separator), &
-      training_configuration%network_configuration_%to_json(), &
-      string_t(separator), &
-      training_configuration%tensor_names_%to_json(), &
-      string_t(footer) &
-    ])
-  end procedure
-
   module procedure default_real_from_file
     training_configuration%file_t = file_object
 
@@ -54,20 +37,7 @@ contains
     end associate
   end procedure
 
-  module procedure double_precision_from_file
-    training_configuration%double_precision_file_t = file_object
-    associate(lines => training_configuration%double_precision_file_t%double_precision_lines())
-      training_configuration%hyperparameters_ = hyperparameters_t(lines)
-      training_configuration%network_configuration_= network_configuration_t(lines)
-      training_configuration%tensor_names_ = tensor_names_t(lines)
-    end associate
-  end procedure
-
   module procedure default_real_to_json
-    json_lines = self%lines()
-  end procedure
-
-  module procedure double_precision_to_json
     json_lines = self%lines()
   end procedure
 
@@ -78,18 +48,7 @@ contains
       lhs%tensor_names_ == rhs%tensor_names_
   end procedure
 
-  module procedure double_precision_equals
-    lhs_eq_rhs = &
-      lhs%hyperparameters_ == rhs%hyperparameters_ .and. &
-      lhs%network_configuration_ == rhs%network_configuration_ .and. &
-      lhs%tensor_names_ == rhs%tensor_names_
-  end procedure
-
   module procedure default_real_mini_batches
-    num_mini_batches = self%hyperparameters_%mini_batches()
-  end procedure
-
-  module procedure double_precision_mini_batches
     num_mini_batches = self%hyperparameters_%mini_batches()
   end procedure
 
@@ -97,15 +56,7 @@ contains
     identifier = self%hyperparameters_%optimizer_name()
   end procedure
 
-  module procedure double_precision_optimizer_name
-    identifier = self%hyperparameters_%optimizer_name()
-  end procedure
-
   module procedure default_real_learning_rate
-    rate = self%hyperparameters_%learning_rate()
-  end procedure
-
-  module procedure double_precision_learning_rate
     rate = self%hyperparameters_%learning_rate()
   end procedure
 
@@ -113,15 +64,7 @@ contains
     nodes = self%network_configuration_%nodes_per_layer()
   end procedure
 
-  module procedure double_precision_nodes_per_layer
-    nodes = self%network_configuration_%nodes_per_layer()
-  end procedure
-
   module procedure default_real_skip_connections
-    using_skip = self%network_configuration_%skip_connections()
-  end procedure
-
-  module procedure double_precision_skip_connections
     using_skip = self%network_configuration_%skip_connections()
   end procedure
 
@@ -142,36 +85,11 @@ contains
     end associate
   end procedure
 
-  module procedure double_precision_activation
-    associate(activation_name => self%network_configuration_%activation_name())
-      select case(activation_name%string())
-        case ("gelu")
-          activation = activation_t(gelu)
-        case ("relu")
-          activation = activation_t(relu)
-        case ("sigmoid")
-          activation = activation_t(sigmoid)
-        case ("swish")
-          activation = activation_t(swish)
-        case default
-          error stop 'activation_factory_s(factory): unrecognized activation name "' // activation_name%string() // '"' 
-      end select
-    end associate
-  end procedure
-
   module procedure default_real_input_names
     input_names = self%tensor_names_%input_names()
   end procedure
 
-  module procedure double_precision_input_names
-    input_names = self%tensor_names_%input_names()
-  end procedure
-
   module procedure default_real_output_names
-    output_names = self%tensor_names_%output_names()
-  end procedure
-
-  module procedure double_precision_output_names
     output_names = self%tensor_names_%output_names()
   end procedure
 
