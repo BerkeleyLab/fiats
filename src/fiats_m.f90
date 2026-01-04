b9b7707113f6e1b1705a2c83a7621dddc65360ab
Damian Rouson <damian@rouson.net>
2026-01-03 23:58:22 -0800

    repro(metadat_{m,s}): rm unused module/submodule

--- src/fiats_m.f90
+++ src/fiats_m.f90
@@ -3,7 +3,6 @@ module fiats_m
  use double_precision_string_m, only : double_precision_string_t
  use hyperparameters_m, only : hyperparameters_t
  use kind_parameters_m, only : default_real, double_precision
- use metadata_m, only : metadata_t
  use training_configuration_m, only : training_configuration_t
  implicit none
 end module fiats_m
