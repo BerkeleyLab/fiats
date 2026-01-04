09962990a77766b295bdd378ebd79580e42929fb
Damian Rouson <damian@rouson.net>
2026-01-04 02:11:22 -0800

    repro(hyperparameters): rm double-prec funcs

--- src/hyperparameters_m.f90
+++ src/hyperparameters_m.f90
@@ -2,7 +2,7 @@
 ! Terms of use are as specified in LICENSE.txt
 module hyperparameters_m
   use julienne_string_m, only : string_t
-  use kind_parameters_m, only : default_real, double_precision
+  use kind_parameters_m, only : default_real
   use double_precision_string_m, only : double_precision_string_t
   implicit none
   private
@@ -14,10 +14,10 @@ module hyperparameters_m
     real(k), private :: learning_rate_ = real(1.5,k)
     character(len=:), allocatable :: optimizer_
   contains
-    generic :: to_json => default_real_to_json, double_precision_to_json
-    procedure, private :: default_real_to_json, double_precision_to_json
-    generic :: operator(==) => default_real_equals, double_precision_equals
-    procedure, private ::      default_real_equals, double_precision_equals
+    generic :: to_json => default_real_to_json
+    procedure, private :: default_real_to_json
+    generic :: operator(==) => default_real_equals
+    procedure, private ::      default_real_equals
   end type
 
   interface hyperparameters_t
@@ -28,26 +28,12 @@ module hyperparameters_m
       type(hyperparameters_t) hyperparameters
     end function
 
-    pure module function double_precision_from_json(lines) result(hyperparameters)
-      implicit none
-      type(double_precision_string_t), intent(in) :: lines(:)
-      type(hyperparameters_t(double_precision)) hyperparameters
-    end function
-
     pure module function default_real_from_components(mini_batches, learning_rate, optimizer) result(hyperparameters)
       implicit none
       integer, intent(in) :: mini_batches
       real, intent(in) :: learning_rate
       character(len=*), intent(in) :: optimizer
       type(hyperparameters_t) hyperparameters
-    end function
-
-    pure module function double_precision_from_components(mini_batches, learning_rate, optimizer) result(hyperparameters)
-      implicit none
-      integer, intent(in) :: mini_batches
-      double precision, intent(in) :: learning_rate
-      character(len=*), intent(in) :: optimizer
-      type(hyperparameters_t(double_precision)) hyperparameters
     end function
 
   end interface
@@ -60,21 +46,9 @@ module hyperparameters_m
       type(string_t), allocatable :: lines(:)
     end function
 
-    pure module function double_precision_to_json(self) result(lines)
-      implicit none
-      class(hyperparameters_t(double_precision)), intent(in) :: self
-      type(string_t), allocatable :: lines(:)
-    end function
-
     elemental module function default_real_equals(lhs, rhs) result(lhs_equals_rhs)
       implicit none
       class(hyperparameters_t), intent(in) :: lhs, rhs
-      logical lhs_equals_rhs
-    end function
-
-    elemental module function double_precision_equals(lhs, rhs) result(lhs_equals_rhs)
-      implicit none
-      class(hyperparameters_t(double_precision)), intent(in) :: lhs, rhs
       logical lhs_equals_rhs
     end function
 
