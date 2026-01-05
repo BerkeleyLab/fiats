d7c662e7e407bfcbf754b455c3439fe014f737f2
Damian Rouson <damian@rouson.net>
2026-01-05 03:26:03 -0800

    repro(string_t): rm type-bound procedure

--- src/julienne_string_m.F90
+++ src/julienne_string_m.F90
@@ -9,9 +9,7 @@ module julienne_string_m
     procedure :: get_json_key
     generic :: operator(==)   => string_t_eq_character
     generic :: assignment(= ) => assign_character_to_string_t
-    generic :: get_json_value => get_string_with_string_key &
-                                ,get_integer
-    procedure, private :: get_string_with_string_key
+    generic :: get_json_value => get_integer
     procedure, private :: get_integer 
     procedure, private :: string_t_eq_character
     procedure, private :: assign_character_to_string_t
@@ -29,12 +27,6 @@ module julienne_string_m
      implicit none
       class(string_t), intent(in) :: self
       type(string_t) unquoted_key
-    end function
-
-    pure module function get_string_with_string_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key, mold
-      type(string_t) :: value_
     end function
 
     pure module function get_integer(self, key, mold) result(value_)
@@ -72,25 +64,6 @@ contains
     associate(opening_key_quotes => index(raw_line, '"'))
       associate(closing_key_quotes => opening_key_quotes + index(raw_line(opening_key_quotes+1:), '"'))
         unquoted_key = string_t(trim(raw_line(opening_key_quotes+1:closing_key_quotes-1)))
-      end associate
-    end associate
-
-  end procedure
-
-  module procedure get_string_with_string_key
-
-    character(len=:), allocatable :: raw_line
-
-    raw_line = self%string()
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(opening_value_quotes => index(text_after_colon, '"'))
-        associate(closing_value_quotes => opening_value_quotes + index(text_after_colon(opening_value_quotes+1:), '"'))
-          if (any([opening_value_quotes, closing_value_quotes] == 0)) then
-            value_ = string_t(trim(adjustl((text_after_colon))))
-          else
-            value_ = string_t(text_after_colon(opening_value_quotes+1:closing_value_quotes-1))
-          end if
-        end associate
       end associate
     end associate
 
