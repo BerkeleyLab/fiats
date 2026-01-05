cd596231f116130d79e0593af1e94911ccb8be61
Damian Rouson <damian@rouson.net>
2026-01-05 04:00:39 -0800

    repro(hyperparameters_t): simplify user-def constructor

--- src/hyperparameters_m.f90
+++ src/hyperparameters_m.f90
@@ -11,11 +11,7 @@ module hyperparameters_m
 
   interface hyperparameters_t
 
-    pure module function default_real_from_json(lines) result(hyperparameters)
-      implicit none
-      type(string_t), intent(in) :: lines(:)
-      type(hyperparameters_t) hyperparameters
-    end function
+    module procedure default_real_from_json
 
   end interface
 
@@ -23,36 +19,27 @@ module hyperparameters_m
 
 contains
 
-  module procedure default_real_from_json
+  pure function default_real_from_json(lines) result(hyperparameters)
+    type(string_t), intent(in) :: lines(:)
+    type(hyperparameters_t) hyperparameters
     integer l
-    logical hyperparameters_key_found
-
-    hyperparameters_key_found = .false.
 
     do l=1,size(lines)
       if (lines(l)%get_json_key() == "hyperparameters") then
-        hyperparameters_key_found = .true.
         hyperparameters%mini_batches_  = lines(l+1)%get_json_value(string_t(mini_batches_key), mold=0)
         return
       end if
     end do
-
-  end procedure
+  end function
 
   pure function to_json(self) result(lines)
     class(hyperparameters_t), intent(in) :: self
     type(string_t), allocatable :: lines(:)
-    character(len=*), parameter :: indent = repeat(" ",ncopies=4)
     integer, parameter :: max_width= 18
     character(len=max_width) mini_batches_string
 
     write(mini_batches_string,*) self%mini_batches_
-
-    lines = [ &
-      string_t(indent // '"hyperparameters": {'), &
-      string_t(indent // indent // '"' // mini_batches_key  // '" : '  // trim(adjustl(mini_batches_string))  // "," ), &
-      string_t(indent // '}') &
-    ]
+    lines = [string_t('"hyperparameters": {"' // mini_batches_key  // '" : '  // trim(adjustl(mini_batches_string)) // '}')]
   end function
 
 end module
