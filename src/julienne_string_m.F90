2462d38d82a80bee404921940f7dd68364a1df34
Damian Rouson <damian@rouson.net>
2026-01-05 03:06:09 -0800

    repro: combine julienne_string_{m,s}

--- src/julienne_string_m.F90
+++ src/julienne_string_m.F90
@@ -104,4 +104,86 @@ module julienne_string_m
 
   end interface
   
+contains
+
+  module procedure as_character
+    raw_string = self%string_
+  end procedure
+
+  module procedure from_characters
+    new_string%string_ = string
+  end procedure
+
+  module procedure get_json_key
+    character(len=:), allocatable :: raw_line
+  
+    raw_line = self%string()
+    associate(opening_key_quotes => index(raw_line, '"'))
+      associate(closing_key_quotes => opening_key_quotes + index(raw_line(opening_key_quotes+1:), '"'))
+        unquoted_key = string_t(trim(raw_line(opening_key_quotes+1:closing_key_quotes-1)))
+      end associate
+    end associate
+
+  end procedure
+
+  module procedure get_string_with_string_key
+
+    character(len=:), allocatable :: raw_line
+
+    raw_line = self%string()
+    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
+      associate(opening_value_quotes => index(text_after_colon, '"'))
+        associate(closing_value_quotes => opening_value_quotes + index(text_after_colon(opening_value_quotes+1:), '"'))
+          if (any([opening_value_quotes, closing_value_quotes] == 0)) then
+            value_ = string_t(trim(adjustl((text_after_colon))))
+          else
+            value_ = string_t(text_after_colon(opening_value_quotes+1:closing_value_quotes-1))
+          end if
+        end associate
+      end associate
+    end associate
+
+  end procedure
+
+  module procedure get_integer
+    character(len=:), allocatable :: raw_line, string_value
+
+    raw_line = self%string()
+    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
+      associate(trailing_comma => index(text_after_colon, ','))
+        if (trailing_comma == 0) then
+          string_value = trim(adjustl((text_after_colon)))
+        else 
+          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
+        end if
+        read(string_value, fmt=*) value_
+      end associate
+    end associate
+
+  end procedure
+
+  module procedure string_t_eq_string_t
+    lhs_eq_rhs = lhs%string() == rhs%string()
+  end procedure
+   
+  module procedure string_t_eq_character
+    lhs_eq_rhs = lhs%string() == rhs
+  end procedure
+
+  module procedure assign_character_to_string_t
+    lhs%string_ = rhs
+  end procedure
+
+  module procedure string_t_cat_string_t
+    lhs_cat_rhs = string_t(lhs%string_ // rhs%string_)
+  end procedure
+   
+  module procedure string_t_cat_character
+    lhs_cat_rhs = string_t(lhs%string_ // rhs)
+  end procedure
+
+  module procedure character_cat_string_t
+    lhs_cat_rhs = string_t(lhs // rhs%string_)
+  end procedure
+   
 end module julienne_string_m
