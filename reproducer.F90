2a8fe0fe4bcc5bb1cf60b608265c32cd47a24494
Damian Rouson <damian@rouson.net>
2026-01-11 15:57:57 -0800

    repro(file_t): rm unnecessary constructors

--- reproducer.F90
+++ reproducer.F90
@@ -155,18 +155,6 @@ module julienne_file_m
 
   interface file_t
 
-    module function from_file_with_string_name(file_name) result(file_object)
-      implicit none
-      type(string_t), intent(in) :: file_name
-      type(file_t) file_object
-    end function
-
-    module function from_file_with_character_name(file_name) result(file_object)
-      implicit none
-      character(len=*), intent(in) :: file_name
-      type(file_t) file_object
-    end function
-
     pure module function from_lines(lines) result(file_object)
       implicit none
       type(string_t), intent(in) :: lines(:)
@@ -185,81 +173,6 @@ contains
 
   module procedure from_lines
     allocate(file_object%lines_, source=lines)
-  end procedure
-
-  module procedure from_file_with_character_name
-    file_object = from_file_with_string_name(string_t(file_name))
-  end procedure
-
-  module procedure from_file_with_string_name
-
-    integer io_status, file_unit, line_num
-    character(len=:), allocatable :: line
-    integer, parameter :: max_message_length=128
-    character(len=max_message_length) error_message
-    integer, allocatable :: lengths(:)
-
-    open(newunit=file_unit, file=file_name%string(), form='formatted', status='old')
-
-    lengths = line_lengths(file_unit)
-
-    associate(num_lines => size(lengths))
-
-      allocate(file_object%lines_(num_lines))
-  
-      do line_num = 1, num_lines
-        allocate(character(len=lengths(line_num)) :: line)
-        read(file_unit, '(a)') line
-        file_object%lines_(line_num) = string_t(line)
-        deallocate(line)
-      end do
-
-    end associate
-
-    close(file_unit)
-
-  contains
-   
-    function line_count(file_unit) result(num_lines)
-      integer, intent(in) :: file_unit
-      integer num_lines
-    
-      rewind(file_unit)
-      num_lines = 0 
-      do  
-        read(file_unit, *, iostat=io_status)
-        if (io_status==iostat_end) exit
-        num_lines = num_lines + 1 
-      end do
-      rewind(file_unit)
-    end function
-
-    function line_lengths(file_unit) result(lengths)
-      integer, intent(in) :: file_unit
-      integer, allocatable ::  lengths(:)
-      integer io_status, l
-      character(len=1) c
-
-      associate(num_lines => line_count(file_unit))
-
-        allocate(lengths(num_lines), source = 0)
-        rewind(file_unit)
-
-        do l = 1, num_lines
-          do
-            read(file_unit, '(a)', advance='no', iostat=io_status, iomsg=error_message) c
-            associate(eliminate_unused_variable_warning => c) ! eliminate NAG compiler "variable c set but never referenced" warning
-            end associate
-            if (io_status==iostat_eor .or. io_status==iostat_end) exit
-            lengths(l) = lengths(l) + 1
-          end do
-        end do
-
-        rewind(file_unit)
-  
-      end associate
-    end function
-
   end procedure
 
 end submodule julienne_file_s
