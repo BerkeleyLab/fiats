9db2eef7ba15bc730a0fa1eb93581144e7738bdb
Damian Rouson <damian@rouson.net>
2026-01-11 15:39:01 -0800

    repro(json): rm unused value getters

--- reproducer.F90
+++ reproducer.F90
@@ -16,23 +16,11 @@ module julienne_string_m
     generic :: operator(==)   => string_t_eq_string_t, string_t_eq_character
     generic :: assignment(= ) => assign_string_t_to_character, assign_character_to_string_t
     generic :: get_json_value => get_string_with_string_key &
-                                ,get_character_with_string_key &
-                                ,get_string_t_array_with_character_key, get_string_t_array_with_string_t_key &
                                 ,get_real &
-                                ,get_logical &
-                                ,get_real_array &
-                                ,get_integer_array &
-                                ,get_integer &
-                                ,get_double_precision
+                                ,get_integer
     procedure, private :: get_string_with_string_key
-    procedure, private :: get_character_with_string_key
-    procedure, private :: get_string_t_array_with_character_key, get_string_t_array_with_string_t_key
     procedure, private :: get_real 
-    procedure, private :: get_logical 
     procedure, private :: get_integer 
-    procedure, private :: get_real_array 
-    procedure, private :: get_integer_array 
-    procedure, private :: get_double_precision 
     procedure, private :: string_t_eq_string_t, string_t_eq_character
     procedure, private :: assign_character_to_string_t
     procedure, private :: string_t_cat_string_t, string_t_cat_character
@@ -72,39 +60,10 @@ module julienne_string_m
       real value_
     end function
 
-    pure module function get_double_precision(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      double precision, intent(in) :: mold
-      double precision value_
-    end function
-
-    pure module function get_character_with_string_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      character(len=*), intent(in) :: mold
-      character(len=:), allocatable :: value_
-    end function
-
     pure module function get_string_with_string_key(self, key, mold) result(value_)
       implicit none
       class(string_t), intent(in) :: self, key, mold
       type(string_t) :: value_
-    end function
-
-    pure module function get_string_t_array_with_string_t_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      type(string_t), intent(in) :: key, mold(:)
-      type(string_t), allocatable :: value_(:)
-    end function
-
-    pure module function get_string_t_array_with_character_key(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self
-      character(len=*), intent(in) :: key
-      type(string_t), intent(in) :: mold(:)
-      type(string_t), allocatable :: value_(:)
     end function
 
     pure module function get_integer(self, key, mold) result(value_)
@@ -112,27 +71,6 @@ module julienne_string_m
       class(string_t), intent(in) :: self, key
       integer, intent(in) ::  mold
       integer value_
-    end function
-
-    pure module function get_logical(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      logical, intent(in) :: mold
-      logical value_
-    end function
-
-    pure module function get_integer_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      integer, intent(in) :: mold(:)
-      integer, allocatable :: value_(:)
-    end function
-
-    pure module function get_real_array(self, key, mold) result(value_)
-      implicit none
-      class(string_t), intent(in) :: self, key
-      real, intent(in) :: mold(:)
-      real, allocatable :: value_(:)
     end function
 
     elemental module function string_t_eq_string_t(lhs, rhs) result(lhs_eq_rhs)
@@ -216,7 +154,6 @@ contains
 
   end procedure
 
-#ifndef NAGFOR
   module procedure get_real
     character(len=:), allocatable :: raw_line, string_value
 
@@ -233,98 +170,7 @@ contains
     end associate
 
   end procedure
-#else
-  module procedure get_real
-    character(len=:), allocatable :: raw_line, string_value, text_after_colon
 
-    raw_line = self%string()
-    text_after_colon = raw_line(index(raw_line, ':')+1:)
-    associate(trailing_comma => index(text_after_colon, ','))
-      if (trailing_comma == 0) then
-        string_value = trim(adjustl((text_after_colon)))
-      else
-        string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-      end if
-      read(string_value, fmt=*) value_
-    end associate
-
-  end procedure
-#endif
-
-#ifndef NAGFOR
-  module procedure get_double_precision
-    character(len=:), allocatable :: raw_line, string_value
-
-    raw_line = self%string()
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(trailing_comma => index(text_after_colon, ','))
-        if (trailing_comma == 0) then
-          string_value = trim(adjustl((text_after_colon)))
-        else
-          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-        end if
-        read(string_value, fmt=*) value_
-      end associate
-    end associate
-
-  end procedure
-#else
-  module procedure get_double_precision
-    character(len=:), allocatable :: raw_line, string_value, text_after_colon
-    integer trailing_comma
-
-    raw_line = self%string()
-    text_after_colon = raw_line(index(raw_line, ':')+1:)
-    associate(trailing_comma => index(text_after_colon, ','))
-      if (trailing_comma == 0) then
-        string_value = trim(adjustl((text_after_colon)))
-      else
-        string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-      end if
-      read(string_value, fmt=*) value_
-    end associate
-
-  end procedure
-#endif
-
-  module procedure get_character_with_string_key
-    associate(string_value => self%get_string_with_string_key(key, string_t(mold)))
-      value_ = string_value%string()
-    end associate
-  end procedure
-
-  module procedure get_string_t_array_with_string_t_key
-    value_ = self%get_string_t_array_with_character_key(key%string(), mold)
-  end procedure
-
-  module procedure get_string_t_array_with_character_key
-
-    character(len=:), allocatable :: raw_line
-    integer i, comma, opening_quotes, closing_quotes
-
-    raw_line = self%string()
-
-    associate(colon => index(raw_line, ':'))
-      associate(opening_bracket => colon + index(raw_line(colon+1:), '['))
-        associate(closing_bracket => opening_bracket + index(raw_line(opening_bracket+1:), ']'))
-          associate(commas => count([(raw_line(i:i)==",", i = opening_bracket+1, closing_bracket-1)]))
-            allocate(value_(commas+1))
-            opening_quotes = opening_bracket + index(raw_line(opening_bracket+1:), '"')
-            closing_quotes = opening_quotes + index(raw_line(opening_quotes+1:), '"')
-            value_(1) = raw_line(opening_quotes+1:closing_quotes-1)
-            do i = 1, commas
-              comma = closing_quotes + index(raw_line(closing_quotes+1:), ',')
-              opening_quotes = comma + index(raw_line(comma+1:), '"')
-              closing_quotes = opening_quotes + index(raw_line(opening_quotes+1:), '"')
-              value_(i+1) = raw_line(opening_quotes+1:closing_quotes-1)
-            end do
-          end associate
-        end associate
-      end associate
-    end associate
-  end procedure
-
-#ifndef NAGFOR
   module procedure get_string_with_string_key
 
     character(len=:), allocatable :: raw_line
@@ -343,63 +189,7 @@ contains
     end associate
 
   end procedure
-#else
-  module procedure get_string_with_string_key
 
-    character(len=:), allocatable :: raw_line, text_after_colon 
-    integer opening_value_quotes
-
-    raw_line = self%string()
-    text_after_colon = raw_line(index(raw_line, ':')+1:)
-    opening_value_quotes = index(text_after_colon, '"')
-    associate(closing_value_quotes => opening_value_quotes + index(text_after_colon(opening_value_quotes+1:), '"'))
-      if (any([opening_value_quotes, closing_value_quotes] == 0)) then
-        value_ = string_t(trim(adjustl((text_after_colon))))
-      else
-        value_ = string_t(text_after_colon(opening_value_quotes+1:closing_value_quotes-1))
-      end if
-    end associate
-
-  end procedure
-#endif
-
-
-#ifndef NAGFOR
-  module procedure get_logical
-    character(len=:), allocatable :: raw_line, string_value
-
-    raw_line = self%string()
-    associate(text_after_colon => raw_line(index(raw_line, ':')+1:))
-      associate(trailing_comma => index(text_after_colon, ','))
-        if (trailing_comma == 0) then
-          string_value = trim(adjustl((text_after_colon)))
-        else 
-          string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-        end if
-        value_ = string_value == "true"
-      end associate
-    end associate
-
-  end procedure
-#else
-  module procedure get_logical
-    character(len=:), allocatable :: raw_line, string_value, text_after_colon
-    integer trailing_comma
-
-    raw_line = self%string()
-    text_after_colon = raw_line(index(raw_line, ':')+1:)
-    trailing_comma = index(text_after_colon, ',')
-    if (trailing_comma == 0) then
-      string_value = trim(adjustl((text_after_colon)))
-    else 
-      string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-    end if
-    value_ = string_value == "true"
-
-  end procedure
-#endif
-
-#ifndef NAGFOR
   module procedure get_integer
     character(len=:), allocatable :: raw_line, string_value
 
@@ -412,50 +202,6 @@ contains
           string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
         end if
         read(string_value, fmt=*) value_
-      end associate
-    end associate
-
-  end procedure
-#else
-  module procedure get_integer
-    character(len=:), allocatable :: raw_line, string_value, text_after_colon
-    integer trailing_comma 
-
-    raw_line = self%string()
-    text_after_colon = raw_line(index(raw_line, ':')+1:)
-    trailing_comma = index(text_after_colon, ',')
-    if (trailing_comma == 0) then
-      string_value = trim(adjustl((text_after_colon)))
-    else 
-      string_value = trim(adjustl((text_after_colon(:trailing_comma-1))))
-    end if
-    read(string_value, fmt=*) value_
-
-  end procedure
-#endif
-
-  module procedure get_integer_array
-    value_ = int(self%get_real_array(key,mold=[0.]))
-  end procedure
-
-  module procedure get_real_array
-    character(len=:), allocatable :: raw_line
-    real, allocatable :: real_array(:)
-    integer i
-
-    raw_line = self%string()
-    associate(colon => index(raw_line, ":"))
-      associate(opening_bracket => colon + index(raw_line(colon+1:), "["))
-        associate(closing_bracket => opening_bracket + index(raw_line(opening_bracket+1:), "]"))
-          i = 0 ! silence a harmless/bogus warning from gfortran
-          associate(commas => count("," == [(raw_line(i:i), i=opening_bracket+1,closing_bracket-1)]))
-            associate(num_inputs => commas + 1)
-              allocate(real_array(num_inputs))
-              read(raw_line(opening_bracket+1:closing_bracket-1), fmt=*) real_array
-              value_ = real_array
-            end associate
-          end associate
-        end associate
       end associate
     end associate
 
