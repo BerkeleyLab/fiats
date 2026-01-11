a8fcb8d6d12f41c60e089be3b8280e2a5fac8bfb
Damian Rouson <damian@rouson.net>
2026-01-11 15:41:34 -0800

    repro: rm unused operator(==) specific proc

--- reproducer.F90
+++ reproducer.F90
@@ -13,7 +13,7 @@ module julienne_string_m
     procedure :: get_json_key
     procedure :: bracket
     generic :: operator(//)   => string_t_cat_string_t, string_t_cat_character, character_cat_string_t
-    generic :: operator(==)   => string_t_eq_string_t, string_t_eq_character
+    generic :: operator(==)   => string_t_eq_character
     generic :: assignment(= ) => assign_string_t_to_character, assign_character_to_string_t
     generic :: get_json_value => get_string_with_string_key &
                                 ,get_real &
@@ -21,7 +21,7 @@ module julienne_string_m
     procedure, private :: get_string_with_string_key
     procedure, private :: get_real 
     procedure, private :: get_integer 
-    procedure, private :: string_t_eq_string_t, string_t_eq_character
+    procedure, private :: string_t_eq_character
     procedure, private :: assign_character_to_string_t
     procedure, private :: string_t_cat_string_t, string_t_cat_character
     procedure, private, pass(rhs) :: character_cat_string_t
@@ -71,12 +71,6 @@ module julienne_string_m
       class(string_t), intent(in) :: self, key
       integer, intent(in) ::  mold
       integer value_
-    end function
-
-    elemental module function string_t_eq_string_t(lhs, rhs) result(lhs_eq_rhs)
-      implicit none
-      class(string_t), intent(in) :: lhs, rhs
-      logical lhs_eq_rhs
     end function
 
     elemental module function string_t_eq_character(lhs, rhs) result(lhs_eq_rhs)
@@ -207,10 +201,6 @@ contains
 
   end procedure
 
-  module procedure string_t_eq_string_t
-    lhs_eq_rhs = lhs%string() == rhs%string()
-  end procedure
-   
   module procedure string_t_eq_character
     lhs_eq_rhs = lhs%string() == rhs
   end procedure
